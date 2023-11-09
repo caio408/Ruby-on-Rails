b30cb8baeb1f6aa1f8ad8f59c2c55069ddbcaa82
Caio César <caiogomes408@gmail.com>
2023-11-08 22:57:40 -0300

    correçções parametro

--- workspace/modulo_02/11_parametro.rb
+++ workspace/modulo_02/11_parametro.rb
@@ -4,25 +4,22 @@ class People
     puts "Inicializando...#{i}"
   end
 end
-        
-    
-    
-  def speak(name = 'pessoal')
-  "Olá, #{name}!"
+    def speak(name = 'pessoal')
+    "Olá, #{name}!"
   end
    
-  def speak2(name = 'pessoal')
-  "Olá, #{name}!"
+    def speak2(name = 'pessoal')
+    "Olá, #{name}!"
   end
    
-  def speak3(text = 'Olá!', text2 = 'Hello!')
-  "#{text} - #{text2}!"
+    def speak3(text = 'Olá!', text2 = 'Hello!')
+    "#{text} - #{text2}!"
   end
 end
 
 p1 = People.new
-puts p1.speak('Olá, fulano de tal!')
-puts p1.speak2('Caio')
-puts p1.speak3('Oi!', 'Yes')
+  puts p1.speak('Olá, fulano de tal!')
+  puts p1.speak2('Caio')
+  puts p1.speak3('Oi!', 'Yes')
 
 p2 = People.new(5)
